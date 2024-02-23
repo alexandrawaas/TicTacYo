146d60fd13e8d6687800d4075c9c921f1f4e1045
awaas <alexandra.waas@hof-university.de>
2024-02-23 19:54:27 +0100

    Put all files in docs folder

--- docs/menuController.js
+++ docs/menuController.js
@@ -6,19 +6,9 @@ viewcontroller.setStartButtonClickListener(() => {
     namePlayer1 = viewcontroller.getPlayer1Input();
     namePlayer2 = viewcontroller.getPlayer2Input();
     rounds = viewcontroller.getRoundsInput();
-    let url = new URL("http://localhost:63342/TicTacYo/docs/game/game.html");
+    let url = new URL("http://localhost:63342/TicTacYo/docs/game.html");
     url.searchParams.set("namePlayer1", namePlayer1);
     url.searchParams.append("namePlayer2", namePlayer2);
     url.searchParams.append("rounds", String(rounds));
     open(url, "_self");
 });
-/**
- Architektur:
- Aufteilung in MenuPage und GamePage, diese haben jeweils als Klassen:
- - View (HTML)
- - Viewcontroller (Variablen getElementById, Methoden für die GUI)
- - Logikcontroller (Nur Spiellogik, vgl. Bowling)
-
- Speichern:
- Cookie (1 Seite) oder Query-Parameter (2 Seiten)
- */ 
--- docs/menuController.ts
+++ docs/menuController.ts
@@ -1,4 +1,4 @@
-import * as viewcontroller from "../menu/menuViewController.js";
+import * as viewcontroller from "./menuViewController.js";
 
 let namePlayer1:string;
 let namePlayer2:string;
@@ -8,7 +8,7 @@ viewcontroller.setStartButtonClickListener(()=>{
     namePlayer1 = viewcontroller.getPlayer1Input();
     namePlayer2 = viewcontroller.getPlayer2Input();
     rounds = viewcontroller.getRoundsInput();
-    let url:URL = new URL("http://localhost:63342/TicTacYo/docs/game/game.html");
+    let url:URL = new URL("http://localhost:63342/TicTacYo/docs/game.html");
     url.searchParams.set("namePlayer1", namePlayer1);
     url.searchParams.append("namePlayer2", namePlayer2);
     url.searchParams.append("rounds", String(rounds));
