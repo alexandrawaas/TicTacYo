d575e87e3c3819e3caff7569473c167139574736
awaas <alexandra.waas@hof-university.de>
2024-02-23 19:59:47 +0100

    Fixed references

--- docs/menuController.js
+++ docs/menuController.js
@@ -6,7 +6,7 @@ viewcontroller.setStartButtonClickListener(() => {
     namePlayer1 = viewcontroller.getPlayer1Input();
     namePlayer2 = viewcontroller.getPlayer2Input();
     rounds = viewcontroller.getRoundsInput();
-    let url = new URL("http://localhost:63342/TicTacYo/docs/game.html");
+    let url = new URL("https://alexandrawaas.github.io/TicTacYo/game.html");
     url.searchParams.set("namePlayer1", namePlayer1);
     url.searchParams.append("namePlayer2", namePlayer2);
     url.searchParams.append("rounds", String(rounds));
